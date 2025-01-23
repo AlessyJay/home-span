a3a285ae292acadb4a041d09874ae8e3bdcce304
BuildTools <alessyraffaello@gmail.com>
2025-01-22 21:23:27 -0700

    Implement tabs

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,80 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+import icons from "@/constants/icons";
+
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: any;
+  title: string;
+}) => (
+  <View className="mt-3 flex flex-1 flex-col items-center">
+    <Image
+      source={icon}
+      tintColor={focused ? "#0061ff" : "#666876"}
+      resizeMode="contain"
+      className="size-6"
+    />
+
+    <Text
+      className={`${focused ? "font-rubikMedium text-primary-300" : "font-rubik text-black-200"} mt-1 w-full text-center text-xs`}
+    >
+      {title}
+    </Text>
+  </View>
+);
+
+const TabLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          position: "absolute",
+          borderTopColor: "#0061ff1a",
+          borderTopWidth: 1,
+          minHeight: 70,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.home} title="Home" focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.search} title="Explore" focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.person} title="Profile" focused={focused} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,18 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-3xl my-10 font-rubik">
+      <Text className="my-10 font-rubik text-3xl font-bold">
         Welcome to LandScaling
       </Text>
-      <Link href="/explore" className="text-2xl my-5">
-        Explore
-      </Link>
-      <Link href="/sign-in" className="text-2xl">
-        SignIn
-      </Link>
-      <Link href="/property/1" className="text-2xl">
-        Property
-      </Link>
     </View>
   );
 }
